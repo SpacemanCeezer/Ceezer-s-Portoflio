a64a74d0b809226949d3f0a91d97f2a9cf58f8f7
Ceezer <cesarocastro321@gmail.com>
2024-12-19 16:07:54 -0500

    fixed resume and download

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -10,36 +10,40 @@ const projectsData = [
     title: 'Recipeasy',
     description: "At Recipeasy, you can find hundreds of delicious and easy-to-make recipes that use ingredients commonly found in most kitchens. Whether you're a seasoned cook or just starting out, this easy-to-use website will be your go-to source for inspiration. Explore our wide range of recipes and start cooking up a storm!",
     image: image1,
+    link: 'https://recipeasy-7c2ada1905ec.herokuapp.com/',
   },
   {
     title: 'Gamehub',
     description: "This project exists to help video game enthusiasts, or video game gifters, make sure they are purchasing a video game title at the best deal possible.",
     image: image2,
+    link: 'https://github.com/crockevin/Game-Hub',
   },
   {
     title: 'SVG Generator',
     description: "This project serves as a generator for an XML-based vector image format. It takes in certain parameters set by the user, such as text and its colors, the shape as well as its colors (also works with hexadeciamls).",
     image: image3,
+    link: 'https://github.com/SpacemanCeezer/SVG-gen',
   },
   {
     title: 'Simple Code Quiz',
     description: "This code quiz will test you javascript knowledge by asking you a series of 5 javscript related questions. The quiz has a timer of 30 seconds that begins once the user has started the quiz. If the answer the user has selected 5 seconds will be deducted from the timer, once the timer has reached 0 or the user has completed the quiz he will get a total score of 5 and be able to save that score.",
     image: image4,
+    link: 'https://github.com/SpacemanCeezer/Code-Quiz',
   },
 ];
 
-const Project = ({ title, description, image }) => {
+const Project = ({ title, description, image, link }) => {
   const ref = useRef(null);
 
   useEffect(() => {
     const observerOptions = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.1
+      threshold: 0.1,
     };
 
-    const handleIntersect = (entries, observer) => {
-      entries.forEach(entry => {
+    const handleIntersect = (entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('visible');
         } else {
@@ -49,25 +53,29 @@ const Project = ({ title, description, image }) => {
     };
 
     const observer = new IntersectionObserver(handleIntersect, observerOptions);
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
+    if (ref.current) observer.observe(ref.current);
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
-      }
+      if (ref.current) observer.unobserve(ref.current);
     };
   }, []);
 
   return (
-    <div className="project fade-in mb-8 flex" ref={ref}>
-      <img src={image} alt={title} className="project-image" />
-      <div className="project-details ml-4">
-        <h3 className="project-title">{title}</h3>
-        <p className="project-description">{description}</p>
+    <a 
+      href={link} 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="project-link"
+      ref={ref}
+    >
+      <div className="project fade-in mb-8 flex">
+        <img src={image} alt={title} className="project-image" />
+        <div className="project-details ml-4">
+          <h3 className="project-title">{title}</h3>
+          <p className="project-description">{description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
@@ -82,6 +90,7 @@ const Projects = () => {
             title={project.title}
             description={project.description}
             image={project.image}
+            link={project.link}
           />
         ))}
       </div>
--- my-portfolio/src/components/Resume.jsx
+++ my-portfolio/src/components/Resume.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import '../styles/main.css';
-import CV from '../assets/cv.png'; 
+import ResumeImage from '../assets/Resume.png';
 
 const Resume = () => {
   const ref = useRef(null);
@@ -12,7 +12,7 @@ const Resume = () => {
       threshold: 0.1,
     };
 
-    const handleIntersect = (entries, observer) => {
+    const handleIntersect = (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('visible');
@@ -23,25 +23,42 @@ const Resume = () => {
     };
 
     const observer = new IntersectionObserver(handleIntersect, observerOptions);
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
+    if (ref.current) observer.observe(ref.current);
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
-      }
+      if (ref.current) observer.unobserve(ref.current);
     };
   }, []);
+
+  const viewFullImage = () => {
+    window.open(ResumeImage, '_blank');
+  };
 
   return (
     <section id="resume" className="section fade-in" ref={ref}>
       <div className="resume-content">
         <h2>Resume</h2>
-        <div className="pdf-container">
-          <img src={CV} alt="Resume" className="resume-pdf" />
+
+        <div className="image-container">
+          <img
+            src={ResumeImage}
+            alt="Resume"
+            className="resume-image"
+            width="100%"
+            onClick={viewFullImage}
+            style={{ cursor: 'pointer' }}
+          />
         </div>
-        <a href={CV} target="_blank" rel="noopener noreferrer" className="resume-download">Download CV</a>
+
+        <div className="button-container">
+        <a
+            href={ResumeImage}
+            download="My_Resume.png" 
+            className="resume-download"
+          >
+            Download Image
+          </a>
+        </div>
       </div>
     </section>
   );
